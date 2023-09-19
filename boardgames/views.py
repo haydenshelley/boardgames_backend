134cc48bae82a85ccf55309cd1bdff80a57aa609
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-09-19 13:41:09 -0700

    make index json

--- boardgames/views.py
+++ boardgames/views.py
@@ -1,15 +1,13 @@
 from django.http import HttpResponse
+from django.http import JsonResponse
 from django.template import loader
 from .models import Boardgame
 
 
 def index(request):
-    latest_boardgame_list = Boardgame.objects.order_by("id")[:5]
-    template = loader.get_template("boardgames/index.html")
-    context = {
-        "latest_boardgame_list": latest_boardgame_list,
-    }
-    return HttpResponse(template.render(context, request))
+    boardgames = Boardgame.objects.all().values('name', 'price', 'player')  # or simply .values() to get all fields
+    boardgames_list = list(boardgames)  # important: convert the QuerySet to a list object
+    return JsonResponse(boardgames_list, safe=False)
 
 def detail(request, boardgame_id):
     return HttpResponse("You're looking at boardgame %s." % boardgame_id)
