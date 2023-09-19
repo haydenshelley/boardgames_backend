bb69dbf68ae2a267d3e4c8857a19ac1ef32f8b27
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-09-19 12:14:46 -0700

    add str method to boardgame

--- boardgames/models.py
+++ boardgames/models.py
@@ -5,3 +5,5 @@ class Boardgame(models.Model):
     name = models.CharField(max_length=50)
     price = models.IntegerField(default=0)
     player = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
