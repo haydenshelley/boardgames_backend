ca72b9427bf74eda365088bb1237053dfa12e200
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-09-19 12:25:45 -0700

    add published recently method to boardgame model

--- boardgames/models.py
+++ boardgames/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 
 class Boardgame(models.Model):
@@ -7,3 +10,5 @@ class Boardgame(models.Model):
     player = models.CharField(max_length=50)
     def __str__(self):
         return self.name
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
--- boardgames/views.py
+++ boardgames/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
+from .models import Boardgame
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the boardgames index.")
+    latest_question_list = Boardgame.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.name for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, boardgame_id):
+    return HttpResponse("You're looking at boardgame %s." % boardgame_id)
